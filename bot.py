b28c5a7ae6c63b7a285a4283d99883c9843edf9f
kodycode <kodythach@gmail.com>
2016-08-12 02:24:50 -0400

    Created a more appropiate name for running the bot

---
+++ bot.py
@@ -0,0 +1,41 @@
+import smtplib
+import imaplib
+import email
+import time
+
+import config
+
+def on_message(listen):
+    command = ''
+    while (1):
+        listen.select(mailbox="Texts")
+        typ, data = listen.search(None, 'UNSEEN', 'FROM', config.phone_address)
+        for num in data[0].split():
+            typ, data = listen.fetch(num, '(RFC822)')
+            body = email.message_from_bytes(data[0][1])
+            for message in body.get_payload():
+                command = str(message)[143:].replace('\n','')
+                print(command)
+                
+        if (command == '!logout'):
+            break;
+        else:
+            time.sleep(5)
+
+def main():
+    listen = imaplib.IMAP4_SSL('imap.gmail.com', port=993)
+    server = smtplib.SMTP('smtp.gmail.com', port=587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(config.bot_username, config.bot_password)
+    listen.login(config.bot_username, config.bot_password)
+    server.sendmail(config.bot_username, config.phone_address, 'Kodybot ON')
+
+    on_message(listen)
+    server.sendmail(config.bot_username, config.phone_address, 'Kodybot OFF')
+    listen.close()
+    listen.logout()
+    server.quit()
+
+main()
